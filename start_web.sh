467a5f981ba7024608de5986b646e939b22b1e4b
Mariano <marianoignaszewski@gmail.com>
2026-05-02 16:16:03 -0300

    Fix: agregar CMD al Dockerfile y collectstatic al start script

--- start_web.sh
+++ start_web.sh
@@ -4,5 +4,6 @@ set -e
 export DJANGO_SETTINGS_MODULE=ERROR_PROOFING.settings
 
 cd CONTROL_HELADERAS
+python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 exec daphne -b 0.0.0.0 -p "$PORT" ERROR_PROOFING.asgi:application
